950086b14c46c50c976842b2e9883023164e2089
MisterBrownRSA <duwayne@hashtec.co.za>
2021-02-05 09:09:45 +0200

    created theatre model, controller, migration and seeder

---
+++ database/seeders/TheatreSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TheatreSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Theatre::create(['cinema_id' => 1, 'allocation' => 1]);
+        Theatre::create(['cinema_id' => 1, 'allocation' => 2]);
+
+        Theatre::create(['cinema_id' => 2, 'allocation' => 1]);
+        Theatre::create(['cinema_id' => 2, 'allocation' => 2]);
+    }
+}
