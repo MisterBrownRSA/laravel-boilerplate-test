76e9d2479b432377ff6d1bcbc7024fbc6e1f1799
MisterBrownRSA <duwayne@hashtec.co.za>
2021-02-05 13:21:45 +0200

    added auth check ternary for the showing vue component

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             @foreach ($showings as $showing)
                 <div class="col-4">
-                    <Showing :showing="{{ $showing->toJson() }}" :auth="{{ Auth::check() }}"></Showing>
+                    <Showing :showing="{{ $showing->toJson() }}" :auth="{{ Auth::check() ? 'true' : 'false' }}" user="{{ Auth::check() ? json_encode(Auth::user()->only('id')) : 'false' }}"></Showing>
                 </div>
             @endforeach
         </div>
