97ee8fcaf35b0c1d77dcb0d07b8c3a5629917df4
MisterBrownRSA <duwayne@hashtec.co.za>
2021-02-05 11:05:32 +0200

    added booking model, controller, seeder and migration

---
+++ app/Models/Booking.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'showing_id',
+        'status'
+    ];
+
+    public function Owner() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function Showing() {
+        return $this->belongsTo(Showing::class);
+    }
+}
---
+++ database/migrations/2021_02_05_084021_create_bookings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('showing_id');
+            $table->string('status'); //string for now, since we won't be using ENUM's right now.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
