220bc43fb552aaa3b9fb4b95ed921c8f8af649be
MisterBrownRSA <duwayne@hashtec.co.za>
2021-02-05 13:21:22 +0200

    added create method

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
 use App\Models\Showing;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    public function index(Request $request, Showing $showing) {
+    public function index(Request $request, Showing $showing)
+    {
         return view('frontend.new_booking')->with(['showing' => $showing]);
+    }
+
+    /**
+     * @param Request $request
+     * @param Showing $showing
+     */
+    public function create(Request $request, Showing $showing)
+    {
+        //TODO:: validation - skipping this step to save time.
+
+        for ($i = 1; $i <= (int) $request->input('tickets'); $i++) {
+            $booking = Booking::create([
+                'user_id' => $request->input('user'),
+                'showing_id' => $showing->id,
+                'status' => 'test',
+            ]);
+        }
+
+        $booking->save();
+        return $booking;
     }
 }
